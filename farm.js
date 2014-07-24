823d4d5806ed59ed82a041c5f5def45b6b16dafe
Damon Oehlman <damon.oehlman@gmail.com>
2014-07-24 15:31:21 +1000

    Use /tmp directory instead of local profiles directory

--- farm.js
+++ farm.js
@@ -1,23 +1,21 @@
 var debug = require('debug')('testbot:farm');
 var spawn = require('child_process').spawn;
 var path = require('path');
-var mkdirp = require('mkdirp');
 var qs = require('querystring');
 var bots = {};
 
 exports.start = function(id, opts, callback) {
   var bot;
-  var profiledir = path.resolve(__dirname, 'profiles', id);
   var args = [
     '--console',
     '--no-first-run',
     '--use-fake-device-for-media-stream', // TODO: customize
     '--use-fake-ui-for-media-stream',
-    '--user-data-dir=' + profiledir
+    '--user-data-dir=/tmp/' + id
   ];
 
   var example = ((opts || {}).example || 'main');
-  var url = 'http://localhost/examples/' + example + '.html?' + qs.stringify(opts);
+  var url = 'http://localhost:3000/examples/' + example + '.html?' + qs.stringify(opts);
 
   var spawnOpts = {
     env: {
@@ -25,16 +23,19 @@ exports.start = function(id, opts, callback) {
     }
   };
 
-  // TODO: kill existing bot if it exists
-
-  mkdirp(profiledir, function(err) {
-    if (err) {
-      return callback(err);
-    }
-
-    debug('bot ' + id + ' url: ' + url);
-    bot = spawn('google-chrome', args.concat([ url ]), spawnOpts);
-
-    callback();
+  debug('bot ' + id + ' url: ' + url);
+  bot = spawn('google-chrome', args.concat([ url ]), spawnOpts);
+  bot.on('close', function(code) {
+    debug('bot ' + id + ' closed, code: ' + code);
   });
+
+  bot.stdout.on('data', function (data) {
+    console.log('stdout: ' + data);
+  });
+
+  bot.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+  });
+
+  callback();
 };
